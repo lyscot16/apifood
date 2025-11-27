421ba6435508e7c6744efc531501273c7c52f80d
alimesino <alimesino@americana.edu.co>
2025-11-27 05:07:40 +0000

    verificacion

--- src/models/company.py
+++ src/models/company.py
@@ -1,5 +1,14 @@
-from src.main import db
 
-class Company(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from src.database import Base
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+
+    products = relationship("Product", back_populates="company")
